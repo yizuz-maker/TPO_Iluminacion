314e5ba7f0107c30d60be2490625bc99553b28ab
yizuz-maker <jesuscambronero2019@gmail.com>
2024-11-08 14:48:34 -0300

    [ADD] ESTIMADO/PRUEBA de OPCIÓN 3: Detalle por Clientes

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ PRODUCTOS = [
     "LUMINARIAS",
 ]
 MODELOS = [1, 2, 3, 4]
-PARAMETROS_ID = [1000, 2000]
+PARAMETROS_ID = [1, 20]
 PRECIO_LAMP_TECHO=[45000,60000,80000,100000]
 PRECIO_LAMP_PARED=[23500,44000,64000,85000]
 PRECIO_LAMP_MESA=[20000,25000,28000,30000]
@@ -119,10 +119,66 @@ def calcularCostoProducto(vendido):
     precio = calcularPrecios(vendido[2],vendido[3])
     return precio // 2
 
+def calcularFacturacionPorCliente(ventas_mes):
+    resumen_clientes = [] # POS[0][0]: ID CLIENTE | POS[0][1]: CANT VENTAS TOTALES | POS[0][2]: TOTAL FACTURADO 
+
+    for i in range(len(ventas_mes)):
+        venta = ventas_mes[i]
+        cliente_id = venta[1]
+        producto = venta[2]
+        modelo = venta[3]
+        precio = calcularPrecios(producto, modelo)
+
+        cliente_existe = False
+        for j in range(len(resumen_clientes)):
+            if resumen_clientes[j][0] == cliente_id:
+                resumen_clientes[j][1] += 1 
+                resumen_clientes[j][2] += precio 
+                cliente_existe = True
+                break
+        
+        if not cliente_existe:
+            resumen_clientes.append([cliente_id, 1, precio]) 
+
+    return resumen_clientes
+
+
+def mostrarResumen(resumen_clientes):
+    for i in range(len(resumen_clientes)):
+        for j in range(i + 1, len(resumen_clientes)):
+            if resumen_clientes[i][2] < resumen_clientes[j][2]:
+                resumen_clientes[i], resumen_clientes[j] = resumen_clientes[j], resumen_clientes[i]
+
+    print("Mes: Agosto 2024")
+    print(f"{'ID cliente':<10} {'Total artículos':<15} {'Total facturado':<15}")
+    for i in range(len(resumen_clientes)):
+        cliente_id = resumen_clientes[i][0]
+        total_articulos = resumen_clientes[i][1]
+        total_facturado = resumen_clientes[i][2]
+        print(f"{cliente_id:<10} {total_articulos:<15} ${total_facturado:<15}")
+
+
 mes = 2 
 anio = 2024
 ventas_mes = generarDatosMes(mes, anio)
 
+# De acá para abajo hay informacion para debuggear
+
+print(ventas_mes)
+for venta in ventas_mes: #Esto tiene que desaparecer, porque sino desapareceemos nosotros
+    print(venta)
+
+
+resumen_clientes = calcularFacturacionPorCliente(ventas_mes)
+print()
+print(resumen_clientes[0][2])
+print()
+for cliente in resumen_clientes:
+    print(cliente)
+print()
+mostrarResumen(resumen_clientes)
+
+"""
 print(CalcularMesFacturado(ventas_mes))
 print(ClientesUnicos(ventas_mes))
-
+"""
